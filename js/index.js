201b22b273095cac625facf10d24b201f5b18ece
Eduardo Dias Bianchini <edudiasbianchini@gmail.com>
2025-05-21 10:12:59 -0300

    refactoring

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// Armazenando os elementos html do index.html em variáveis
+const addListBtn = document.getElementById('add-list-btn')
+const listnameInput = document.getElementById('input-listname')
+const alertMessage = document.querySelector('.alert')
+const removeAlert = document.getElementById('remove-notification-btn')
+
+// Arrow Function para iniciar um bloco de código ao receber o evento de click no botão de Adicionar Item
+addListBtn.addEventListener('click', () => {
+    // Resgata o valor digitado no input e remove os espaços em branco com o trim()
+    const listname = listnameInput.value.trim()
+
+    // Caso o valor digitado seja diferente de "vazio", esse valor é armazenado no localStorage
+    if (listname !== "") {
+        localStorage.setItem("listName", listname)
+        window.location.href = "./pages/list.html";
+    }
+    // Sendo igual a "vazio", irá forçar o aparecimento de um alerta para o usuário.
+    else {
+        alertMessage.style.display = 'flex'
+
+        listnameInput.classList.add('input-alert-border')
+
+        // Adiciona um Timeout de 3000ms, fazendo o alerta desaparecer após esse intervalo
+        setTimeout(() => {
+            alertMessage.style.display = 'none'
+            listnameInput.classList.remove('input-alert-border');
+        }, 3000)
+
+        // Remove o alerta ao receber o evento de click no botão de remoção
+        removeAlert.addEventListener('click', () => {
+            alertMessage.style.display = 'none'
+        })
+    }
+})
--- js/list.js
+++ js/list.js
@@ -1,26 +1,30 @@
-// Getting the HTML elements and storing them in variables
-const inputTask = document.getElementById('input-item')
+// Obtendo os elementos html necessários e armazenando em variáveis
+const inputItem = document.getElementById('input-item')
 const addBtn = document.getElementById('add-btn')
 const removeMessageDiv = document.querySelector('.remove-message')
-const taskContainer = document.querySelector('.list')
+const itemContainer = document.querySelector('.list')
 const trashIcon = document.getElementById('trash-icon')
 const removeNotificationBtn = document.getElementById('remove-notification-btn')
+const listTitle = document.getElementById('list-title')
+
+// Armazena a variável que conterá o valor digitado no input do index.html (correspondente ao nome da tarefa)
 const listNameFromHome = localStorage.getItem("listName")
+listTitle.textContent = listNameFromHome + " | Quicklist"
 const listName = document.getElementById('quicklist-name')
 
 // Atribui o nome da lista informada pelo usuário no input do home.html
 listName.textContent = listNameFromHome
 
-// Executa a função createItem() ao receber o click 
+// Arrow function para executar o bloco de código diretamente ao receber o evento de click no botão
 addBtn.addEventListener('click', () => {
     // Resgata o valor digitado no input e remove os espaços em branco com o trim()
-    const listValue = inputTask.value.trim()
+    const itemValue = inputItem.value.trim()
 
-    // Caso o usuário digita algo diferente de "", o bloco de código é executado para criação de um item na lista
-    if (listValue !== "") {
-        // Div principal das tasks
-        const divTasks = document.createElement('div')
-        divTasks.className = 'item'
+    // Caso seja digitado algo diferente de "", o bloco de código é executado para criação de um item na lista
+    if (itemValue !== "") {
+        // Div principal para os itens da lista
+        const divItems = document.createElement('div')
+        divItems.className = 'item'
 
         // Div contendo o input:checkbox e o nome do item da lista
         const CheckAndTaskname = document.createElement('div')
@@ -31,16 +35,16 @@ addBtn.addEventListener('click', () => {
         inputCheckbox.type = 'checkbox'
         inputCheckbox.className = 'item-check'
 
-        // Criando o span, usando o textContent para indicar que o valor de texto do span será o item digitado no inputTask
+        // Criando o span e usando textContent para informar o nome do item digitado
         const itemName = document.createElement('span')
-        itemName.textContent = listValue;
+        itemName.textContent = itemValue;
 
         // Adicionando o input:checkbox e o span na div 'check-and-taskname'
         CheckAndTaskname.appendChild(inputCheckbox)
         CheckAndTaskname.appendChild(itemName)
 
         // Adicionando a div 'check-and-taskname' dentro da div 'task'
-        divTasks.appendChild(CheckAndTaskname)
+        divItems.appendChild(CheckAndTaskname)
 
         // Criando a div que envolve o botão de exclusão do item
         const removeTaskBtnDiv = document.createElement('div')
@@ -49,7 +53,7 @@ addBtn.addEventListener('click', () => {
         // Criando o elemento img que servirá como botão para fechar a notificação de exclusão
         const trashIcon = document.createElement('img')
         trashIcon.className = 'trash-icon'
-        trashIcon.src = './public/image/Frame.svg'
+        trashIcon.src = '../public/image/trash.svg'
 
         // Criando o botão que envolve a imagem acima
         const removeBtn = document.createElement('button')
@@ -58,25 +62,24 @@ addBtn.addEventListener('click', () => {
         // Adicionando o removeBtn dentro da div 'remove-item-btn'
         removeTaskBtnDiv.appendChild(trashIcon);
 
-        // Adicionando a div 'remove-item-btn' dentro da div 'task'
-        divTasks.appendChild(removeTaskBtnDiv);
-
+        // Adicionando a div 'remove-item-btn' dentro da div 'items'
+        divItems.appendChild(removeTaskBtnDiv);
 
         // Limpa o conteudo do input após toda a execução de código acima
-        inputTask.value = ""
+        inputItem.value = ""
 
-        // Adicionando a div 'task' dentro da div 'tasks-div', que envolve toda a lista
-        taskContainer.appendChild(divTasks);
+        // Adicionando a div 'task' dentro da div 'item', que envolve toda a lista
+        itemContainer.appendChild(divItems);
 
         // Arrow Function para aplicar o display 'flex' na div que representa a notificação de exclusão de item
         trashIcon.addEventListener('click', () => {
-            taskContainer.removeChild(divTasks)
+            itemContainer.removeChild(divItems)
             removeMessageDiv.style.display = 'flex'
 
-            // Adiciona um intervalo de tempo no qual a notificação fica em tela. Após 3000ms o display 'none' é aplicado novamente
+            // Adiciona um intervalo de tempo (2000ms) no qual a notificação fica em tela
             setTimeout(() => {
                 removeMessageDiv.style.display = 'none'
-            }, 3000)
+            }, 2000)
 
             // Quanto o botão de exclusão recebe o evento de click, o display é setado como 'none'
             removeNotificationBtn.addEventListener('click', () => {
